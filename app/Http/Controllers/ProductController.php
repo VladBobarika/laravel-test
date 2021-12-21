7717d37164337940d27567858ed1e060df7f2a52
VladBobarika <vlad.bobarika@gmail.com>
2021-12-21 15:34:07 +0300

    init commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index($id) {
+        $product = Product::findOrFail($id);
+        $products = Product::all();
+        return view('product.product', [
+            'product' => $product,
+            'products' => $products
+        ]);
+    }
+
+    public function catalog() {
+        $offset = 0;
+        $categories = Category::all();
+        $brands = Brand::all();
+        $products = Product::where('status',1)->simplePaginate(10);
+        return view('product.store', [
+            'products' => $products,
+            'categories' => $categories,
+            'brands' => $brands
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Brand;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('main');
+});
+
+
+Route::get('admin', function () {
+    return view('admin.index');
+});
+
+
+Route::resources([
+    'brand' => \App\Http\Controllers\Admin\BrandController::class,
+    'category' => \App\Http\Controllers\Admin\CategoryController::class,
+    'product' => \App\Http\Controllers\Admin\ProductController::class
+]);
+
+
+Route::get('product/store', function () {
+    return view('store');
+});
+
+Route::get('/product', function () {
+    $category = new Category();
+    $category->name = 'Mobile phone';
+    $category->status = true;
+    $category->save();
+
+    $data = [
+        'name' => 'Xiaomi mi 11',
+        'status' => true
+    ];
+    $category = Category::create($data);
+    return view('product');
+});
+
+Route::get('catalog', [ProductController::class, 'catalog']) ->name('catalog');
+
+Route::get('hello', [SiteController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
